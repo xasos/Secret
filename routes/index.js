549ad6f663ce370e926c30e09102bcd5b09b6eee
Niraj Pant <nirajpant7@gmail.com>
2014-12-30 10:17:59 -0600

    stuff

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// var cheerio = require('cheerio');
-// var request = require('request');
+var cheerio = require('cheerio');
+var request = require('request');
 
 var baseURL = 'http://secret.ly/'
 
@@ -13,17 +13,43 @@ router.get('/', function(req, res) {
 router.get('/secrets/:category', function(req, res) {
 	var category = req.params.category;
 	if (category) {
-		baseURL+=category;
+		baseURL+= "c/" + category;
 	}
+	console.log(baseURL);
+
+	request(baseURL, function(err, resp, body) {
+		if (!err && resp.statusCode == 200) {
+			console.log("inside");
+			var $ = cheerio.load(body);
+			$('id-collection-secret').each(function(i) {
+				var secret = $(this).find('.center-fix').eq(0).text().trim();
+				console.log(secret);
+			});
+
+		}
+	});
+
 
 	res.send({test: "test"})
 });
 
 router.get('/p/:id', function(req, res) {
 	var permalink = req.params.id;
-	if (category) {
-		baseURL+= "p/" + category;
+	if (permalink) {
+		baseURL+= "p/" + permalink;
 	}
+
+	request(baseURL, function(err, resp, body) {
+		if (!err && resp.statusCode == 200) {
+			var $ = cheerio.load(body);
+			console.log($);
+			$('id-collection-secret collection-secret').each(function(i) {
+				var secret = $(this).find('.center-fix').eq(0).text().trim();
+				console.log(secret);
+			});
+
+		}
+	})
 
 	res.send({test: "test"})
 });
